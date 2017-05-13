489448c99770335a4803e6b1105c83cfc09b815c
zjk <mac@zjk-macshare.local>
2017-05-13 23:00:23 +0800

    no message

--- Api/Demo/Controller/TestController.class.php
+++ Api/Demo/Controller/TestController.class.php
@@ -17,7 +17,7 @@ class TestController extends ApiController{
 
     public function index(){
         $data = I('post.');
-
+        echo $data;exit;
         $this->mtReturn('请求参数',200, $data);
 //        $va = M('feedback')->select();
 //        if($va) $this->myApiPrint('success',200,$va);
