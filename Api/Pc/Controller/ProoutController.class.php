aa6c77c8d09f32b72c471b5d0f02b9a9e15210e7
zjk <mac@zjk-macshare.local>
2017-07-11 22:45:34 +0800

    no message

--- Api/Pc/Controller/ProoutController.class.php
+++ Api/Pc/Controller/ProoutController.class.php
@@ -52,7 +52,7 @@ class ProoutController extends CommonController
         $data['sjiage'] = $postData['jpsjiage'];
         $data['yufu'] = $postData['yufu'];
         $data['zhekou'] = $postData['zhekou'];
-        $data['paystatus'] = $postData['paystatus'];
+        //$data['paystatus'] = $postData['paystatus'];
         $data['outtime'] = gettime();
         $data['status'] = $postData['status'];
     	$pro = A('pro');
