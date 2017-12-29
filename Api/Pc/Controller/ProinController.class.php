e1947e50705469a807f0228c902520da61f30fb8
zjk <mac@zjk-macshare.local>
2017-12-29 17:43:48 +0800

    no message

--- Api/Pc/Controller/ProinController.class.php
+++ Api/Pc/Controller/ProinController.class.php
@@ -102,7 +102,7 @@ class ProinController extends CommonController
         $proin = $model->where(array("jpid" => $data['jpid']))->select();
         $isSuccess = true;
         $id = $proin[0]['id'];
-        Log::write($id,'notice');
+        Log::write(json_encode($proin),'notice');
         if(empty($id) || $id == 0){
             $isSuccess = $model->add($data);
         } else {
