cc64f1f17c5cf990e47674576e4bcb2f8a62b0d8
zjk537 <zjk537@126.com>
2019-12-16 00:15:52 +0800

    test

--- Api/Pc/Controller/ProController.class.php
+++ Api/Pc/Controller/ProController.class.php
@@ -261,11 +261,12 @@ class ProController extends CommonController
         // 更新进货记录
         if($data['status'] === '在库'){
             // 超管 店长可查看进价
-            $posArrName = array('超管', '店长');
-            if (!in_array($this->curUser['uid'], C('ADMINISTRATOR')) && !in_array(trim($this->curUser['posname']), $posArrName))
-            {
-                unset($data["jpjiage"]);
-            }
+            // $posArrName = array('超管', '店长');
+            // if (!in_array($this->curUser['uid'], C('ADMINISTRATOR')) && !in_array(trim($this->curUser['posname']), $posArrName))
+            // {
+            //     unset($data["jpjiage"]);
+            // }
+            $data['jpjiage'] = $data['jiage'];
             //$data['remark']  = '';
             $proin           = A('Proin');
             $proin->autoAdd($data);
