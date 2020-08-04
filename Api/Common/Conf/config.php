7f4be68867aa2ee00e9d0d00949a7b85a042be6e
zjk537 <zjk537@126.com>
2020-08-04 11:35:01 +0800

    update database

--- Api/Common/Conf/config.php
+++ Api/Common/Conf/config.php
@@ -4,7 +4,7 @@ return array(
     //数据库设置
     'DB_TYPE'               =>  'mysqli',     // 数据库类型
     'DB_HOST'               =>  'localhost', // 服务器地址
-    'DB_NAME'               =>  'v2crm',          // 数据库名
+    'DB_NAME'               =>  'v2crm_pre',          // 数据库名
     'DB_USER'               =>  'root',      // 用户名
     'DB_PWD'                =>  '1qaz@WSX',          // 密码
     'DB_PORT'               =>  '3306',        // 端口
