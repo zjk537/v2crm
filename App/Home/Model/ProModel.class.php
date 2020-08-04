e856b575975529904154c1f3d7579f1c6014b672
zjk537 <zjk537@126.com>
2020-08-04 10:24:57 +0800

    update test

--- App/Home/Model/ProModel.class.php
+++ App/Home/Model/ProModel.class.php
@@ -21,17 +21,17 @@ class ProModel extends Model{
     
 		// 自动完成规则
 	protected $_auto = array (
-	    array('status','在库',1), // 对status字段在新增的时候赋值0
+		array('status','在库',1), // 对status字段在新增的时候赋值0
 		array('uid','getuserid',1,'function'),
-        array('uname','gettruename',1,'function'),
-        array('depid','getdepid',1,'function'),	
-        array('depname','getdepname',1,'function'),	
-        array('depphone','getdepphone',1,'function'),
-	    array('addtime','gettime',1,'function'), 
-	    array('updatetime','gettime',1,'function'), 
+		array('uname','gettruename',1,'function'),
+		array('depid','getdepid',1,'function'),	
+		array('depname','getdepname',1,'function'),	
+		array('depphone','getdepphone',1,'function'),
+		array('addtime','gettime',1,'function'), 
+		array('updatetime','gettime',1,'function'), 
 		array('uuid','getuserid',2,'function'),
-        array('uuname','gettruename',2,'function'), 		
-	    array('updatetime','gettime',2,'function'), 
+		array('uuname','gettruename',2,'function'), 		
+		array('updatetime','gettime',2,'function'), 
 	);
 
 }
