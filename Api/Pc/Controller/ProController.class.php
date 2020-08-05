7f7c8668aa523dc70c2023437a2ad80229935976
zjk537 <zjk537@126.com>
2020-08-05 14:44:50 +0800

    测试权限控制

--- Api/Pc/Controller/ProController.class.php
+++ Api/Pc/Controller/ProController.class.php
@@ -142,7 +142,7 @@ class ProController extends CommonController
             $asName = $prefix.$value;
             $authField = 'home/'. $prefix . '/dbfields'; // 功能列表中配置的 商品属性 客户属性
             if(in_array($asName, C('AUTH_FIELDS')) && !authcheck($authField, $this->curUser['uid'])){
-                $newValue = "'' as `". $asName ."`";
+                $newValue = "NULL as `". $asName ."`";
             } else {
                 $newValue = '`' . C('DB_PREFIX') . $prefix . '`.`' . $value . '` as `' . $asName . '`';
             }
